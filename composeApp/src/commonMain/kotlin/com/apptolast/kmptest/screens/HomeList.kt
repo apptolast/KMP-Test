7696124a28db51c0ceb51163b9e8115650ab98e1
Alberto Hidalgo <hgarcia.alberto@gmail.com>
2025-03-05 23:57:01 +0000

    feat: Improve HomeList screen and Android preview
    -   **MainActivity:**
        - Added `showBackground = true` to the `AppAndroidPreview` annotation for better visibility.
    -   **HomeList:**
        - Updated `HomeList` to display a list of items using `LazyColumn` and `items`.
        - Created a `ListItem` composable to render each item, including click handling via `toast`.
        - Updated items with padding, style, and click events.
        - Added a `listItems` companion object with data to populate the list.

--- composeApp/src/commonMain/kotlin/com/apptolast/kmptest/screens/HomeList.kt
+++ composeApp/src/commonMain/kotlin/com/apptolast/kmptest/screens/HomeList.kt
@@ -1,36 +1,58 @@
 package com.apptolast.kmptest.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import com.apptolast.kmptest.interfaces.toast
 
 class HomeList : Screen {
-//    override val key = uniqueScreenKey
 
     @Composable
     override fun Content() {
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            item {
-                Text(
-                    text = "Test 1",
-                    modifier = Modifier.clickable {
-                        toast("Item click 1")
-                    }
-                )
-            }
-            item {
-                Text(
-                    text = "Test 2",
-                    modifier = Modifier.clickable {
-                        toast("Item click 2")
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+
+            items(listItems) { item ->
+                ListItem(
+                    text = item,
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    onClick = { text ->
+                        toast("click: $text")
                     }
                 )
             }
         }
     }
+
+
+    companion object {
+        private val listItems = listOf("Item 1", "Item 2", "Item 3")
+    }
 }
+
+
+@Composable
+fun ListItem(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = {},
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.subtitle2,
+        modifier = modifier.clickable { onClick(text) }
+    )
+}
+
+
